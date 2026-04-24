c9efd68557bcbb9752f3eab81ddb6b49a8fd6eb7
Divyang gupta <guptadivyang12345@gmail.com>
2026-04-24 18:33:48 +0530

    initialize git repositry and upload project

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://streamify-app-ii41.onrender.com/api";
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true, // send cookies with the request
+});
