0a79b7f5f84ac82c071088aa4afa347c65dfdf38
DivyangGupta1305 <guptadivyang12345@gmail.com>
2026-04-24 19:08:05 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001/api"
-    : "https://streamify-app-ii41.onrender.com/api";
+    : "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
